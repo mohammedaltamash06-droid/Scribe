5a1db9e953b4b58b6e23ef643395bee4a62aabd5
m-ohamme-d <72089696+m-ohamme-d@users.noreply.github.com>
2025-08-30 18:39:51 +0000

    my new changes

---
+++ whisper.cpp/build/examples/bench/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/Scribe/whisper.cpp/examples/bench
+# Build directory: /workspaces/Scribe/whisper.cpp/build/examples/bench
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ whisper.cpp/build/ggml/src/CMakeFiles/ggml-cpu.dir/ggml-cpu/hbm.cpp.o.d
@@ -0,0 +1,3 @@
+ggml/src/CMakeFiles/ggml-cpu.dir/ggml-cpu/hbm.cpp.o: \
+ /workspaces/Scribe/whisper.cpp/ggml/src/ggml-cpu/hbm.cpp \
+ /usr/include/stdc-predef.h
