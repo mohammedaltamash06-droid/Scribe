d590a75ef1a068a4f7983d65a5e3f3adafc8033f
m-ohamme-d <72089696+m-ohamme-d@users.noreply.github.com>
2025-08-30 19:04:47 +0000

    feat: describe what you changed

--- whisper.cpp/build/examples/bench/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/Scribe/whisper.cpp/examples/bench
-# Build directory: /workspaces/Scribe/whisper.cpp/build/examples/bench
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- whisper.cpp/build/tests/CMakeFiles/test-vad.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for test-vad.
-# This may be replaced when dependencies are built.
